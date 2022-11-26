91d2fc49cc3e278705dc77cd8b7a34e000d1f091
zhu_qi <3236093774@qq.com>
2022-11-26 13:20:59 +0800

    feat:项目初始化

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
+import './assets/css/reset.css'
 import router from './router'
 import store from './store'
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div class="home"></div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+  export default {
+    name: 'HomeView',
+    components: {}
   }
-}
 </script>
+
+<style scoped>
+  .home {
+    width: 7.5rem;
+    height: 200px;
+    background-color: pink;
+  }
+</style>
